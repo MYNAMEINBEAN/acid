677cf50b7af833674b6896be841b8c37870215c4
BEAN <BEANBOSSSSSSSSS@GMAIL.COM>
2025-07-23 12:52:12 -0400

    Initial commit from GUI

---
+++ public/a/static/uv/uv.config.js
@@ -0,0 +1,10 @@
+self.__uv$config = {
+    prefix: "/static/proll/",
+    bare: "https://r2fh2387hv2b2rfbiz.site",
+    encodeUrl: Ultraviolet.codec.xor.encode,
+    decodeUrl: Ultraviolet.codec.xor.decode,
+    handler: "/static/uv/uv.handler.js",
+    bundle: "/static/uv/uv.bundle.js",
+    config: "/static/uv/uv.config.js",
+    sw: "/static/uv/uv.sw.js",
+};
