5efd93df887bcd7b19d50afbad3eb03c6bbc5c3e
MYNAMEINBEAN <bmb6@birmingham.k12.mi.us>
2025-07-25 14:46:57 -0400

    Update uv.config.js

--- public/a/static/uv/uv.config.js
+++ public/a/static/uv/uv.config.js
@@ -3,8 +3,8 @@ self.__uv$config = {
     bare: "https://r2fh2387hv2b2rfbiz.site",
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
-    handler: "/static/uv/uv.handler.js",
-    bundle: "/static/uv/uv.bundle.js",
-    config: "/static/uv/uv.config.js",
-    sw: "/static/uv/uv.sw.js",
+    handler: "/a/static/uv/uv.handler.js",
+    bundle: "/a/static/uv/uv.bundle.js",
+    config: "/a/static/uv/uv.config.js",
+    sw: "/a/static/uv/uv.sw.js",
 };
